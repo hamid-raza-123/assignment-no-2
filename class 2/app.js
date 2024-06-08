97ccde1dc6af344d071e33307f0b44e780c5b3ff
hamid-raza-123 <149107223+hamid-raza-123@users.noreply.github.com>
2024-06-08 17:04:20 +0500

    Add files via upload

---
+++ class 2/app.js
@@ -0,0 +1,40 @@
+
+let exp = "faraz" || (true + false) + 5 && " " || "true";
+console.log("I. exp:", exp);
+
+let b = 3;
+let exp1 = (1 + 2) + ++b || 5 && 0;
+console.log("II. exp1:", exp1);
+
+let a = 5;
+let exp2 = 32 && true - ++a && " " || "true";
+console.log("III. exp2:", exp2);
+
+let exp3 = (true + 3) * 5 && undefined || "faraz" + 5;
+console.log("IV. exp3:", exp3);
+
+let exp4 = 32 + "abc" || (true + false) + false || +5 || "true";
+console.log("V. exp4:", exp4);
+
+let exp5 = false || (true + true) + 15 && "" || 50;
+console.log("VI. exp5:", exp5);
+
+let exp6 = (true + false) + 5 || false + 8 + "abc" || "true";
+console.log("VII. exp6:", exp6);
+
+let exp7 = "faraz" || false + 10 || "true";
+console.log("VIII. exp7:", exp7);
+
+let exp8 = 12 + (false + false) + true && null || "faraz";
+console.log("IX. exp8:", exp8);
+
+let exp9 = (false + true + false + 2) || "faraz" && false || 1 + 10;
+console.log("X. exp9:", exp9);
+
+let exp10 = (true + false) + 5 || false + 8 + "abc" || "true";
+console.log("XI. exp10:", exp10);
+
+
+ 
+
+  
